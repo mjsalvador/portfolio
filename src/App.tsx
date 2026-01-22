bbbe2ef6eb18b88bacf1db5a293684e030afbb56
mjsalvador <matthew.jsalvador@gmail.com>
2026-01-22 17:17:32 +0900

    adds projects section

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import SiteHeader from "@/components/site-header";
 import Hero from "@/components/hero";
 import AboutMe from "@/components/about-me";
 import Footer from "@/components/footer"
+import Projects from "@/components/projects";
 
 function App() {
   return (
     <>
       <SiteHeader />
       <Hero />
+      <Projects />
       <AboutMe />
       <Footer />
     </>
