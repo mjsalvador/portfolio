2099c9acca84946d8891168cc6c971428be04c85
mjsalvador <matthew.jsalvador@gmail.com>
2026-01-23 15:39:15 +0900

    updates font style in hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,7 +3,7 @@ export default function Hero() {
     <section className="pt-32 pb-20 px-8 min-h-screen flex flex-col justify-center text-stone-900">
       <div className="max-w-4xl">
         {/* Main Headline */}
-        <div className="mb-8 tracking-tight">
+        <div className="mb-8 tracking-tight font-light">
           <h1 className="leading-tight">Hey there, I'm Matthew Salvador</h1>
           <p>
             a Software Engineer focused on delivering{" "}
@@ -15,7 +15,7 @@ export default function Hero() {
 
         {/* Description */}
         <div className="mb-12 max-w-6xl">
-          <p className="text-3xl font-light leading-tight">
+          <p className="text-3xl font-light tracking-tight leading-tight">
             Bridging the gap between modern-day technology and a resilient
             future. Over 5 years of experience building enterprise and
             consumer-facing products in the automotive and education industries.
@@ -25,7 +25,7 @@ export default function Hero() {
         </div>
 
         {/* CTA Buttons */}
-        <div className="flex flex-col">
+        <div className="flex flex-col tracking-tight">
           <a href="#projects" className="underline transition-colors">
             Projects↓
           </a>
